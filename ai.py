0488d27474b38d9fedd1aed8b3d79777ae5baea9
Tairi Delgado <trdelgado19@gmail.com>
2022-11-05 20:41:27 -0700

    Remove text to cleanup psuedocode

--- ai.py
+++ ai.py
@@ -1,7 +1,5 @@
-# MalSoul
-# Malanimus
-
 # AI algorithm
+
 # Assess utility of current state
 # For each available action
 	# For each available set of targets
@@ -12,68 +10,3 @@
 # take action
 # record immediate reward/state
 # update model
-
-# Assert
-
-# Business Plan
-# B2B
-	# Career/Role
-	# Industry/Sector
-	# Customer/Client
-	# User
-# B2C
-	# Career/Role
-	# Customer/User
-
-# Awareness
-	# Channels (hub & spoke)
-		# Website
-		# Social Media 
-			# Youtube
-			# LinkedIn
-			# Instragram
-			# Facebook
-			# Twitter
-		# Professional Networks
-			# Livermore Chamber of Commerce
-			# Discord
-				# AI
-				# Datos
-			# Slack
-				# Tri-Valley Professional Networks
-				# Techqueria
-			# Personal
-				# Jessie Martinez
-				# Sebastían
-				# Luis Serrano
-				# Paty 
-				# 
-		# AI, Python, Data Science Conferences
-# Like
-	# Channels
-		# Social Media posts
-			# nano digital workshops & tutorials
-			# 2-3 minute
-		# Physical Meet and Greets
-
-# Trust
-	# 
-# Try
-	# 
-# Buy
-	# 
-# Recommend
-	#
-
-# Customer Survey
-# Interest/Hobbies/
-# Painpoints
-# Aspirations
-# Career/Role
-# Current Knowledge/Experiences/Skills
-# Future Knowledge/Experiences/Skills
-# Geographic Location
-# Industry/Sector
-# Customer/Client/User
-
-# https://arstechnica.com/tech-policy/2022/08/the-merge-the-biggest-change-in-ethereum-history-explained/
