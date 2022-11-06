cc111b63cfeb2dbb42ca932baf89aeed80ac7aa9
Tairi Delgado <trdelgado19@gmail.com>
2022-11-05 19:46:36 -0700

    Remove outdated display player stat code

--- battleHandlerIO.py
+++ battleHandlerIO.py
@@ -49,66 +49,19 @@ class BattleHanderIO(object):
         # List of player attributes
         att_list = ['name', 'health', 'mana', 'speed', 'accuracy']
         
-        # Print enemy stats
+        # Enemy Stats 
         self.title_menu("Enemy Stats")
         self.player_attributes_row(att_list)
-
-        '''enemy_name = "|\tName\t|\t"
-        enemy_health = "|\tHealth\t|\t"
-        enemy_health_list = []
-        enemy_mana = "|\tMana\t|\t"
-        enemy_mana_list = []'''
-        
-        # For each enemy get the name and health
-        '''for player in enemies.get_teammembers():
-                enemy = enemies.get_teammember(player)
-                enemy_name += enemy.get_name() + "\t|\t"
-                enemy_health += "{:.2f}" + "\t|\t"
-                enemy_health_list.append(enemy.get_health())
-                enemy_mana += "{:.2f}" + "\t|\t"
-                enemy_mana_list.append(enemy.get_mana())'''
 
         # Display enemy stats
         self.team_stats(enemies)
 
-                
-        # Display stats
-        '''print(enemy_name)
-        print(self.window_length*"=")
-        print(enemy_health.format(*enemy_health_list))
-        print(self.window_length*"=")
-        print(enemy_mana.format(*enemy_mana_list))
-        print(self.window_length*"=")'''
-
-
-        # Print hero stats
+        # Hero stats
         self.title_menu("Hero Stats")
         self.player_attributes_row(att_list)
-        '''hero_name = "|\tName\t|\t"
-        hero_health = "|\tHealth\t|\t"
-        hero_health_list = []
-        hero_mana = "|\tMana\t|\t"
-        hero_mana_list = []'''
-        
-        # For each hero get the name, health, and mana
-        '''for player in heroes.get_teammembers():
-                hero = heroes.get_teammember(player)
-                hero_name += hero.get_name() + "\t|\t"
-                hero_health += "{:.2f}" + "\t|\t"
-                hero_health_list.append(hero.get_health())
-                hero_mana += "{:.2f}" + "\t|\t"
-                hero_mana_list.append(hero.get_mana())'''
-                
-        # Display stats
-        '''print(hero_name)
-        print(self.window_length*"=")
-        print(hero_health.format(*hero_health_list))
-        print(self.window_length*"=")
-        print(hero_mana.format(*hero_mana_list))'''
 
         # Display hero stats
         self.team_stats(heroes)
-
 
         # Display who is current active player
         active_player_string = "{}'s turn".format(active_player.get_name())
